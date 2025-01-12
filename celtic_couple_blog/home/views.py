9acad164aabab80d5bf8ea150f2805e90b04d1aa
CleanDesignUK <cleandesign.uk@gmail.com>
2025-01-12 12:55:35 +0000

    update

--- celtic_couple_blog/home/views.py
+++ celtic_couple_blog/home/views.py
@@ -1,6 +1,17 @@
 # home/views.py
 
 from django.shortcuts import render
+from blog.models import BlogPage
 
 def home_view(request):
-    return render(request, 'home/home_page.html')
+    # Attempt to fetch the latest blog post by date
+    featured_post = (
+        BlogPage.objects.live()
+        .order_by('-date')
+        .first()
+    )
+
+    context = {
+        'featured_post': featured_post,
+    }
+    return render(request, 'home/home_page.html', context)
