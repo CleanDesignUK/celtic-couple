bf0ad20c1977ef3bf12232a517a8be4e95443769
CleanDesignUK <cleandesign.uk@gmail.com>
2025-01-13 05:15:33 +0000

    update

--- celtic_couple_blog/blog/models.py
+++ celtic_couple_blog/blog/models.py
@@ -58,6 +58,7 @@ class BlogPageGalleryImage(Orderable):  # Inherit from Orderable
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250, blank=True, null=True)  # **New Subtitle Field**
     body = RichTextField(blank=True)
     author = models.ForeignKey(
         'blog.Author',
@@ -74,6 +75,7 @@ class BlogPage(Page):
             FieldPanel('author'),
         ], heading="Blog Details"),
         FieldPanel('intro'),
+        FieldPanel('subtitle'),  # **Add Subtitle to Content Panels**
         FieldPanel('body', classname="full"),
         FieldPanel('tags'),
         InlinePanel('gallery_images', label="Gallery Images"),
