1daf06827cd486353a8ca3867b3f8d0695c4ffed
CleanDesignUK <cleandesign.uk@gmail.com>
2025-01-14 09:03:36 +0000

    update

--- celtic_couple_blog/settings/production.py
+++ celtic_couple_blog/settings/production.py
@@ -2,20 +2,30 @@ from .base import *
 import dj_database_url
 
 DEBUG = False
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-production-secret-key')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
+# Replace with your actual secret key
+SECRET_KEY = 'q*d)3!g-59j1k118-b177mx4p1gdd152r2%#2f#3ss'
+
+# Allowed hosts
+ALLOWED_HOSTS = ['celticcouple.com', 'celticcouple.uk', 'celtic-blog-10cb8fe7bf21.herokuapp.com']
+
+# Database configuration
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'qYkwbmDnqJeRkvAgXwyvCMCWOVdgijkO',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '5432',
+    }
 }
 
+# SSL and security settings
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Logging for production
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
